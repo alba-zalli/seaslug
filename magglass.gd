6a6ee4802fd7e70e4038202057cf37d09c7a90c9
alba <130872707+alba-zalli@users.noreply.github.com>
2026-06-30 16:16:00 -0400

    zooming magnifying glass

--- magglass.gd
+++ magglass.gd
@@ -3,6 +3,16 @@ extends Node2D
 var dragging := false
 var drag_offset := Vector2.ZERO
 
+@onready var viewport = $SubViewport
+@onready var lens = $TextureRect
+@onready var cam = $SubViewport/Camera2D
+
+func _process(delta):
+	cam.global_position = global_position
+
+func _ready():
+	lens.texture = viewport.get_texture()
+
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
